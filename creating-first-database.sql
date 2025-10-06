1a5b343c2dabc12adb1a3ed15f6cf61a9a2405f8
Ana Letícia <59629110+analeticiadss@users.noreply.github.com>
2025-10-06 15:34:04 -0400

    Update creating-first-database.sql
    aula 4 de melhorando a estrutura do database do curso em vídeo

--- creating-first-database.sql
+++ creating-first-database.sql
@@ -1,16 +1,23 @@
-#creating my first database
+create database cadastro
+default character set utf8mb4
+default collate utf8mb4_general_ci; # pra ter os caracteres que vou utilizar com o idioma pt-br
 
-create database cadastro; #creates a database called "cadastro"
-
-use cadastro; # im getting the created database to use
-
+use cadastro;
 create table pessoas(
-  name varchar(30),
-  idade tinyint,
-  sexo char(1),
-  peso float,
-  altura float,
-  nacionalidade varchar(20)
-); # creating a table called "pessoas"
+	id int not null auto_increment,
+	nome varchar(30) not null,
+    nascimento date,
+    sexo enum('M', 'F'), # apenas duas opções M ou F
+    peso decimal(5,2), # primeiro numero = total de digitos, segundo num = numeros depois da virgula
+    altura decimal(3,2),
+    nacionalidade varchar(20) default 'Brasil',
+    primary key(id)
+) default charset = utf8mb4;
 
-describe pessoas; # it shows the table created and its variables
+
+# not null: obriga a pessoa a digitar um nome pra uma pessoa
+# decimal(5,2) -> exemplo: 111,22 -> 5 digitos, com dois numeros depois da virgula (22)
+# default 'Brasil' = se ninguem digitar nada, por default, a pessoa vai ter nacionalidade do BR
+# chave primaria: permite que exista um ID unico pra cada pessoa -> para não repetir tuplas dentro de uma tabela
+# drop table ~nome-da-tabela~ = apaga a tabela
+# drop database ~nome-do-banco-de-dados~ = apaga o database
